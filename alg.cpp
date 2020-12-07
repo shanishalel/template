89dd6f98ca052aeca5e5c9fbefdd69c68ac3d171
Shani <shanishalel89@gmail.com>
2020-12-07 13:29:23 +0200

    finished fibunnatci

--- alg.cpp
+++ alg.cpp
@@ -1,25 +1,30 @@
 #include <iterator>
 #include <iostream>
 #include <vector>
+#include <math.h>
 #include <algorithm>
 #include "alg.h"
 
 using namespace std;
 
-template <typename it1,typename it2 ,typename func > it2 Transform2
-(it1 b, it1 e, it2 b2, func f ){
-  while(b!=e){
-    *b2=f(*(b),*(b+1));
-    b2++;b=b+2;
-  }
-  return b2;
-  
-}
+//from geeksforgeeks
+bool isPerfectSquare(int x) 
+{ 
+    int s = sqrt(x); 
+    return (s * s == x); 
+} 
 
+//from geeksforgeeks
+bool isFibonacci(int n) 
+{ 
+    // n is Fibinacci if one of 5*n*n + 4 or 5*n*n - 4 or both 
+    // is a perferct square 
+    return isPerfectSquare(5 * n * n + 4) || isPerfectSquare(5 * n * n - 4); 
+} 
 template<typename T, typename R>
 bool Fib (T a, R b) {
 	while(a != b){
-        if(a+(a++)==(a+2)){
+        if(isFibonacci(*a)){
             a++;
         }
         else{
@@ -33,6 +38,7 @@ bool Fib (T a, R b) {
 
 
 int main(){
+    //checking transpose
     // int ia[]={1,2,3,4,5,6,7,8};
     // int *p = Transpose(begin(ia),end(ia));
     // //one before
@@ -49,8 +55,20 @@ int main(){
     //     }
     // }
     
-    int ia[]={1,2,3,10,8,6};
-    vector<int> vec(100); 
-    auto p = Transform2(begin(ia), end(ia), vec.begin(),[](const int x, const int y){return x + y;});
-    cout<<*(p-3)<<endl;
+    //checking transform2
+    // int ia[]={1,2,3,10,8,6};
+    // vector<int> vec(100); 
+    // auto p = Transform2(begin(ia), end(ia), vec.begin(),[](const int x, const int y){return x + y;});
+    // cout<<*(p-3)<<endl;
+
+    //checking fibbunatchi
+    // int ia[] = {144};
+    // bool f = Fib(begin(ia), end(ia));
+    // if(f==true){
+    //     cout<<"true"<<endl;
+    // }else{
+    //     cout<<"false"<<endl;
+    // }
+    
+    
 }
